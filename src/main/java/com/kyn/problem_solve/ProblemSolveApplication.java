9f1e8c94eb4869761a85b1e492ac19969e77225b
kyn-317 <kynauto0317@gmail.com>
2025-03-27 20:24:19 +0900

    first commit

---
+++ src/main/java/com/kyn/problem_solve/ProblemSolveApplication.java
@@ -0,0 +1,13 @@
+package com.kyn.problem_solve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemSolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemSolveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyn/problem_solve/ProblemSolveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyn.problem_solve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemSolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
